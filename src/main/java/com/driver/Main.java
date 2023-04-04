e6ec7b5c1fc95f5f3a8b973004f7988d29e07786
91961 <aakashparab98@gmail.com>
2023-04-04 18:58:41 +0530

    1st Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String []args)
+    {
+        RWOnly obj = new RWOnly();
+        //obj.name="Swabhiman";  //Will give an error
+
+        obj.setName("Swabhiman");
+        String name=obj.getName();
+
+        System.out.println(name);
+    }
+
 }
